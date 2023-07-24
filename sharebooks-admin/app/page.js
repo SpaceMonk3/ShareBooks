42babde091b92a5e12a2927aceb71e2a02ea4412
Dheer Tammina <dtammina@gmu.edu>
2023-07-25 01:06:20 +0530

    initial project code

---
+++ sharebooks-admin/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="bg-blue-900 w-screen h-screen flex items-center"> 
+      <div className="text-center w-full">
+        <button className="bg-white p-2 px-4 rounded-lg">Login in Google</button>
+      </div>
+    </div>
+  )
+}
