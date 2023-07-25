429bc326d8958c34e9f0d0aeccf88f5fb8bd4f79
Dheer Tammina <dtammina@gmu.edu>
2023-07-25 11:32:55 +0530

    reinstalled project with yarn

--- sharebooks-admin/app/page.js
+++ sharebooks-admin/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center"> 
+    <div className="bg-blue-900 w-screen h-screen flex items-center">
       <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">Login in Google</button>
+        <button className="bg-white p-2 px-4 rounded-lg"> Login with Google </button>
       </div>
     </div>
   )
